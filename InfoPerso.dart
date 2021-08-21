bd4a0c9146517a893a12d5d0017a735f115f4597
0rmindo <86755009+0rmindo@users.noreply.github.com>
2021-08-20 22:22:26 -0300

    Primeira versão
    Primeira versão

--- InfoPerso.dart
+++ InfoPerso.dart
@@ -19,35 +19,12 @@ class _InfoScreenState extends State<InfoScreen> {
     );
   }
 
-  Widget showInstructionMessage(textTab) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Você não tem $textTab',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Material(
       child: DefaultTabController(
         length: 2,
-        child: Scaffold(
-          
+        child: Scaffold(          
           // Barra superior do app
           appBar: AppBar(
             backgroundColor: Color(0xffffffff),
@@ -71,7 +48,6 @@ class _InfoScreenState extends State<InfoScreen> {
               color: Color(0xff000000),
             ),
           ),
-
           // Botões de caracteristicas
           ButtonColumn(
             alignment: MainAxisAlignment.SpacingEvenly,
@@ -80,11 +56,11 @@ class _InfoScreenState extends State<InfoScreen> {
                 // Botões de Gênero
                 child const Text('Gênero:'),
                 children: Row(
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Masculino'),
                   ),
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Feminino'),
                   ),
@@ -92,36 +68,34 @@ class _InfoScreenState extends State<InfoScreen> {
                 // Botões de Idade
                 child const Text('Idade:'),
                 children: Row(
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Bebê'),
                   ),
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Criança'),
                   ),
                 ),
                 children: Row(
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Jovem'),
                   ),
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Adulto'),
                   ),
                 ),
                 children: Row(
-                  ButtonBar(
+                  OutlineButton(
                     onPressed: _showToast,
                     child: const Text('Idoso'),
                   ),
                 ),
               ),
-              
-
-            ),
-
+            ],
+          ),
           actions: [
             TextButton(
               onPressed: () {
@@ -146,4 +120,126 @@ class _InfoScreenState extends State<InfoScreen> {
       ),
     );
   }
+}
+import 'package:flutter/material.dart';
+
+class InfoScreen extends StatefulWidget {
+  const InfoScreen({Key? key}) : super(key: key);
+
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+    return result;
+  }
+
+  Widget createAvatarGrid(avatars) {
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(15),
+      children: getFormatedAvatars(avatars),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(          
+          // Barra superior do app
+          appBar: AppBar(
+            backgroundColor: Color(0xffffffff),
+            shadowColor: Color.fromRGBO(0, 0, 0, 0),
+            titleSpacing: 0,
+            // Titulo da barra superior (Informações)
+            title: Text( 
+              'Informações',
+              style: TextStyle(
+                color: Color(0xff000000),
+              ),
+            ),
+            // Botão para voltar para home
+            bottom: (
+              labelPadding: EdgeInsets.symmetric(vertical: 7),
+              labelColor: Color(0xff0077B6),
+              labelStyle: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            leading: Icon(
+              Icons.arrow_back_outlined,
+              color: Color(0xff000000),
+            ),
+          ),
+          // Botões de caracteristicas
+          ButtonColumn(
+            alignment: MainAxisAlignment.SpacingEvenly,
+            children: <Widget>[
+              children: Column(
+                // Botões de Gênero
+                child const Text('Gênero:'),
+                children: Row(
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Masculino'),
+                  ),
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Feminino'),
+                  ),
+                ),
+                // Botões de Idade
+                child const Text('Idade:'),
+                children: Row(
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Bebê'),
+                  ),
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Criança'),
+                  ),
+                ),
+                children: Row(
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Jovem'),
+                  ),
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Adulto'),
+                  ),
+                ),
+                children: Row(
+                  OutlineButton(
+                    onPressed: _showToast,
+                    child: const Text('Idoso'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Navigation to "/new" route
+              },
+
+              // Botão de Avançar
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('AVANÇAR'),
+                      SizedBox(width: 10),
+                      Icon(Icons.chevron_right_outlined),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+        ),
+      ),
+    );
 }
